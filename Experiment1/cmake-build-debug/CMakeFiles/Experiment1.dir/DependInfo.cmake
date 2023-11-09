c12501d88a2ae9cd8eedc69d48559ccb3968ace8
mo <1952698848@qq.com>
2023-11-09 20:45:27 +0800

    run

---
+++ Experiment1/cmake-build-debug/CMakeFiles/Experiment1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Project/Homework/algorithm/Experiment1/force.cpp" "CMakeFiles/Experiment1.dir/force.cpp.obj" "gcc" "CMakeFiles/Experiment1.dir/force.cpp.obj.d"
+  "D:/Project/Homework/algorithm/Experiment1/generatePoints.cpp" "CMakeFiles/Experiment1.dir/generatePoints.cpp.obj" "gcc" "CMakeFiles/Experiment1.dir/generatePoints.cpp.obj.d"
+  "D:/Project/Homework/algorithm/Experiment1/main.cpp" "CMakeFiles/Experiment1.dir/main.cpp.obj" "gcc" "CMakeFiles/Experiment1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Experiment1/cmake-build-debug/CMakeFiles/Experiment1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Experiment1.dir/force.cpp.obj"
+  "CMakeFiles/Experiment1.dir/force.cpp.obj.d"
+  "CMakeFiles/Experiment1.dir/generatePoints.cpp.obj"
+  "CMakeFiles/Experiment1.dir/generatePoints.cpp.obj.d"
+  "CMakeFiles/Experiment1.dir/main.cpp.obj"
+  "CMakeFiles/Experiment1.dir/main.cpp.obj.d"
+  "Experiment1.exe"
+  "Experiment1.exe.manifest"
+  "Experiment1.pdb"
+  "libExperiment1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Experiment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
